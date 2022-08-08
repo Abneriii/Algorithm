5acfdadb8fa13444b1e786835d20c1ff52f9ba17
Abner <1569209119@qq.com>
2022-08-08 15:52:03 +0800

    common updates

--- src/Others/LongestConsecutiveSequence.java
+++ src/Others/LongestConsecutiveSequence.java
@@ -1,5 +1,9 @@
 package Others;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Author:
  * Created at:2022/8/8
@@ -12,6 +16,39 @@ package Others;
 public class LongestConsecutiveSequence {
 
 
+
+
+
+/*
+
+LC.128.最长连续序列
+2022.3.19：不会。------看题解，懂了，自己代码实现了。
+
+*/
+
+    static class Solution {
+        public int longestConsecutive(int[] nums) {
+            if(nums.length==0) return 0;
+            Arrays.sort(nums);
+            Map<Integer,Integer> map=new HashMap<>();
+            for(int a:nums){
+                if(!map.containsKey(a)){
+                    map.put(a,1);
+                }
+
+            }
+            int maxLength=1;
+            for(int a:nums){
+                if(map.containsKey(a-1)){
+                    map.put(a,map.get(a-1)+1);
+                    maxLength=Math.max(map.get(a-1)+1,maxLength);
+                }
+
+            }
+            return maxLength;
+
+        }
+    }
 
 
 }
